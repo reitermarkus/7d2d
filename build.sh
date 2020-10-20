e2061b40b7b80d645ace5be2f338c7a559f42dbe
Markus Reiter <me@reitermark.us>
2020-10-20 23:23:03 +0200

    Update to Alpha 19.2.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-versions=(alpha19.1 alpha18.4 alpha17.4)
+versions=(alpha19.2 alpha18.4 alpha17.4)
 
 for version in "${versions[@]}"; do
   docker build --build-arg "VERSION=${version}" -t "reitermarkus/7d2d:${version}" .
